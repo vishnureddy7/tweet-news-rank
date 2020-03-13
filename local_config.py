1ce955ca113eb74fd5d4fddd2d7ab975b47478a4
Guru Vishnu Vardhan Reddy <guruvishnu.desireddy@redbus.com>
2020-03-13 17:37:59 +0530

    added local_config.py file

---
+++ local_config.py
@@ -0,0 +1,106 @@
+# consumer key
+CONSUMER_KEY = "TWITTER_CONSUMER_KEY"
+# consumer secret
+CONSUMER_SECRET = "TWITTER_CONSUMER_SECRET"
+# app key
+ACCESS_KEY = "TWITTER_ACCESS_KEY"
+# app secret
+ACCESS_SECRET = "TWITTER_ACCESS_SECRET"
+
+# Country name with their languages
+langs = {'ar': 'Arabic',
+         'bg': 'Bulgarian',
+         'bn': 'Bengali',
+         'ca': 'Catalan',
+         'cs': 'Czech',
+         'da': 'Danish',
+         'de': 'German',
+         'el': 'Greek',
+         'en': 'English',
+         'hi': 'Hindi',
+         'fa': 'Persian',
+         'fi': 'Finnish',
+         'fr': 'French',
+         'es': 'Spanish',
+         'hr': 'Croatian',
+         'hu': 'Hungarian',
+         'id': 'Indonesian',
+         'is': 'Icelandic',
+         'it': 'Italian',
+         'iw': 'Hebrew',
+         'ja': 'Japanese',
+         'ko': 'Korean',
+         'kn': 'Kannada',
+         'lt': 'Lithuanian',
+         'lv': 'Latvian',
+         'ms': 'Malay',
+         'nl': 'Dutch',
+         'no': 'Norwegian',
+         'pl': 'Polish',
+         'pt': 'Portuguese',
+         'ro': 'Romanian',
+         'ru': 'Russian',
+         'sk': 'Slovak',
+         'sl': 'Slovenian',
+         'sr': 'Serbian',
+         'sv': 'Swedish',
+         'ta': 'Tamil',
+         'te': 'Telugu',
+         'th': 'Thai',
+         'tl': 'Filipino',
+         'tr': 'Turkish',
+         'uk': 'Ukrainian',
+         'ur': 'Urdu',
+         'vi': 'Vietnamese',
+         'et': 'Estonian',
+         'zh_CN': 'Chinese(simplified)',
+         'zh_TW': 'Chinese (traditional)'}
+
+indian_langauges = ['hi', 'te', 'ta', 'kn', 'en']
+tracks = ['india', "movie", 'cricket', 'sports', 'hockey', 'football', 'politics', 'modi', 'jagan', 'AP', 'capital',
+          'trump', 'google', 'facebook', 'hyderabad', 'bangalore', 'chennai', 'kurnool', 'dhoni', 'kohli', 'rohit',
+          'music', 'news', 'indian news', 'timesnow', 'indiatoday', 'america', 'usa', 'csk', 'rahul gandhi', 'technology',
+          'python', 'java', 'javascript', 'c++', 'republic', 'bbc', 'bbl', 'ipl', 'icc', 'bcci', 'asia', 'budget',
+          'pakistan', 'tech', 'trending', 'election', 'stocks', 'software', '']
+
+# # List of countries
+countries_list = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Antigua and Barbuda', 'Argentina',
+                  'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+                  'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan',
+                  'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brazil',
+                  'Brunei', 'Bulgaria', 'Burma (Myanmar)', 'Cambodia', 'Cameroon', 'Canada',
+                  'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros',
+                  'Congo', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark',
+                  'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt',
+                  'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji',
+                  'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana',
+                  'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guyana', 'Haiti', 'Honduras',
+                  'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel',
+                  'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati',
+                  'Korea', 'Korea', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia',
+                  'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania',
+                  'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali',
+                  'Malta', 'Marshall Islands', 'Mauritania', 'Mexico', 'Micronesia', 'Moldova', 'Monaco',
+                  'Mongolia', 'Montenegro', 'Morocco', 'Nauru', 'Nepal', 'Netherlands',
+                  'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan',
+                  'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines',
+                  'Poland', 'Portugal', 'Qatar', 'Romania', 'Russian Federation',
+                  'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa',
+                  'San Marino', 'Saudi Arabia', 'Senegal', 'Serbia', 'Singapore', 'Slovakia', 'Slovenia',
+                  'Solomon Islands', 'Somalia', 'South Africa', 'South Sudan', 'Spain',
+                  'Sri Lanka', 'Sudan', 'Suriname', 'Sweden', 'Switzerland', 'Syria', 'Tajikistan',
+                  'Tanzania', 'Thailand', 'Tonga', 'Trinidad and Tobago', 'Tunisia',
+                  'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates',
+                  'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu',
+                  'Vatican City', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe']
+
+# swear or abusive words
+swear_words = ["bastard", "fuck", "shit", "bitch", "idiot", "piss off", "asshole", "damn", "cunt", "bollocks", "bugger"]
+# love or positive words
+love_words = ["love", "thank", "happy", "bless", "affection", "adore", "beautiful", "angel", "friend"]
+
+# list of programming languages
+programing_lang_list = ["Java", "JavaScript", "PHP", "Python", "Ruby", "C++", "CPP", "Node.Js", "Erlang", "GoLang"]
+
+# Database file name
+db = "data/tweet-data.db"
